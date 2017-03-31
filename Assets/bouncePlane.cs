23056668d5512b7e710fb2d47b321ec4341f881b
Easton <eastonself@gmail.com>
2017-03-30 20:40:08 -0400

    Adding Project to Git

---
+++ Assets/bouncePlane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouncePlane : MonoBehaviour {
+	float speed = 1f;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(0f,Mathf.PingPong(Time.time*speed, 1), 0f);
+	}
+}
