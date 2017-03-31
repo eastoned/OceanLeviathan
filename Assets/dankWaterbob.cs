f19c9e27ce499026b69ac86034c0dc296e3c295b
Unknown <tgr235@nyu.edu>
2017-03-30 22:07:00 -0400

    watermechanics1.1

---
+++ Assets/dankWaterbob.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dankWaterbob : MonoBehaviour {
+
+	public float time; // the duration of each animation
+	public float high;
+
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveBy (this.gameObject, iTween.Hash ("y", high, "time", time, "looptype", "pingpong", "easetype", iTween.EaseType.easeInOutSine));
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
